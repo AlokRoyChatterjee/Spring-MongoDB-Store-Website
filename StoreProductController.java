913f1d898bfde146970ac87139cf38d292ad4963
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-27 18:44:11 -0400

    Update StoreProductController.java

--- StoreProductController.java
+++ StoreProductController.java
@@ -1,6 +1,5 @@
 
 package storeproducts;
-
 import com.springmvc.springmongodbweb.models.Product;
 import com.springmvc.springmongodbweb.repositories.ProductRepository;
 import java.util.Optional;
@@ -14,9 +13,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StoreProductController {
-
     @Autowired
     StoreItemRepository itemrepository;
+     @RequestMapping("/save")
+    public String save(@RequestParam String itemname, @RequestParam String infoitem, @RequestParam Double itemamount, @RequestParam String item) {
+        StoreItem item = new StoreIem();
+        item.setitemanme(itemname);
+        item.setiteminfo(infoitem);
+        item.setitemamount(prodPrice);
+        item.setitem(item);
+        productRepository.save(item);
+
+        return "redirect:/show/" + product.getId();
+    }
 
     @RequestMapping("/product")
     public String product(Model model) {
@@ -24,52 +33,41 @@ public class StoreProductController {
         return "StoreItem";
     }
 
-    @RequestMapping("/create")
-    public String create(Model model) {
-        return "addstoreproduct";
-    }
-
-    @RequestMapping("/save")
-    public String save(@RequestParam String prodName, @RequestParam String prodDesc, @RequestParam Double prodPrice, @RequestParam String prodImage) {
-        StoreItem product = new StoreIem();
-        product.setProdName(prodName);
-        product.setProdDesc(prodDesc);
-        product.setProdPrice(prodPrice);
-        product.setProdImage(prodImage);
-        productRepository.save(product);
-
-        return "redirect:/show/" + product.getId();
-    }
-
-    @RequestMapping("/show/{id}")
-    public String show(@PathVariable String id, Model model) {
-        model.addAttribute("product", itemrepository.findById(id).get());
-        return "storeproductsdisplay";
-    }
-
-    @RequestMapping("/delete")
-    public String delete(@RequestParam String id) {
-        Optional<Product> product = itemrepository.findById(id);
-        itemrepository.delete(product.get());
-
-        return "redirect:/product";
-    }
-    
-    @RequestMapping("/edit/{id}")
+   
+      @RequestMapping("/edit/{id}")
     public String edit(@PathVariable String id, Model model) {
         model.addAttribute("product", itemrepository.findById(id).get());
         return "ChangeStoreItem";
     }
-    
-    @RequestMapping("/update")
-    public String update(@RequestParam String id, @RequestParam String prodName, @RequestParam String prodDesc, @RequestParam Double prodPrice, @RequestParam String prodImage) {
-        Optional<StoreItem> product = itemrepository.findById(id);
-        product.get().setProdName(prodName);
-        product.get().setProdDesc(prodDesc);
-        product.get().setProdPrice(prodPrice);
-        product.get().setProdImage(prodImage);
-        productRepository.save(product.get());
+      @RequestMapping("/update")
+    public String update(@RequestParam String id, @RequestParam String itemname, @RequestParam String infoitem, @RequestParam Double itemamount, @RequestParam String item) {
+        Optional<StoreItem> item = itemrepository.findById(id);
+        item.get().setitemname(itemname);
+        item.get().setinfoitem(infoitem);
+        item.get().setitemamount(itemamount);
+        item.get().setitem(item);
+        itemRepository.save(item.get());
 
-        return "redirect:/show/" + product.get().getId();
+        return "redirect:/show/" + item.get().getId();
     }
+   
+     @RequestMapping("/create")
+    public String create(Model model) {
+        return "addstoreproduct";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(@RequestParam String id) {
+        Optional<StoreItem> product = itemrepository.findById(id);
+        itemrepository.delete(product.get());
+        return "redirect:/product";
+    }
+    
+       @RequestMapping("/show/{id}")
+    public String show(@PathVariable String id, Model model) {
+        model.addAttribute("product", itemrepository.findById(id).get());
+        return "storeproductsdisplay";
+    }
+    
+  
 }
