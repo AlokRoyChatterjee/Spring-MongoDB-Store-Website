7ea5eb40a1776cbfcc5eceec704e971a082b2fc3
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-27 18:39:15 -0400

    Update StoreItem.java

--- StoreItem.java
+++ StoreItem.java
@@ -2,68 +2,58 @@ package storeitems;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/**
- *
- * @author didin
- */
 @Document(collection = "products")
 public class StoreItem {
     @Id
     String id;
-    String prodName;
-    String prodDesc;
-    Double prodPrice;
-    String prodImage;
-
-    public StoreItem() {
-    }
-
-    public StoreItem(String prodName, String prodDesc, Double prodPrice, String prodImage) {
-        this.prodName = prodName;
-        this.prodDesc = prodDesc;
-        this.prodPrice = prodPrice;
-        this.prodImage = prodImage;
+    String itemname;
+    String infoitem;
+    Double itemamount;
+    String item;
+   public StoreItem(String itemname, String infoitem, Double itemamount, String item) {
+        this.itemname = prodName;
+        this.infoitem = prodDesc;
+        this.itemamount = prodPrice;
+        this.item = prod;
     }
 
     public String getId() {
         return id;
     }
+    public void setitemname(String itemname) {
+        this.itemname = itemname;
+    }
+    public String getitem() {
+        return item;
+    }
 
+    public void setitem(String item) {
+        this.item = item;
+    }
+
+    public String getinfoitem() {
+        return prodDesc;
+    }
+
+    public void setiteminfo(String infoitem) {
+        this.infoitem = infoitem;
+    }
     public void setId(String id) {
         this.id = id;
     }
 
-    public String getProdName() {
-        return prodName;
+    public String getitemname() {
+        return itemname;
     }
 
-    public void setProdName(String prodName) {
-        this.prodName = prodName;
+    public Double getitemamount() {
+        return itemamount;
     }
 
-    public String getProdDesc() {
-        return prodDesc;
+    public void setitemamount(Double itemamount) {
+        this.itemamount = itemamount;
     }
 
-    public void setProdDesc(String prodDesc) {
-        this.prodDesc = prodDesc;
-    }
-
-    public Double getProdPrice() {
-        return prodPrice;
-    }
-
-    public void setProdPrice(Double prodPrice) {
-        this.prodPrice = prodPrice;
-    }
-
-    public String getProdImage() {
-        return prodImage;
-    }
-
-    public void setProdImage(String prodImage) {
-        this.prodImage = prodImage;
-    }
-    
+   
     
 }
